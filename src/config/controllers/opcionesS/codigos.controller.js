88fb679d58cf0f9c0c9eeb5af8000e66f7268bf9
AvendanoisPepe <jsavendano62@gmail.com>
2024-01-11 16:39:51 -0500

    fin de la logica

--- src/config/controllers/opcionesS/codigos.controller.js
+++ src/config/controllers/opcionesS/codigos.controller.js
@@ -4,6 +4,7 @@ import '@styles/view_codigos.scss'
 // import { DB_Guiones_InfoYMedios } from '@javascript/API/guiones.infoymedios.js'
 
 import { buscarElemento, copiarValor } from '@javascript/funcionales/funcionesGlobales.js'
+import { BD_Codigos } from '@javascript/BD/BD_Codigos.js'
 import { popOk, popCardGuionBig } from '@javascript/funcionales/alertas.js'
 
 export default () => {
@@ -11,33 +12,69 @@ export default () => {
     subdocument.className = 'hook'
     subdocument.innerHTML = Hook + Codigos
     const stylesParentElemInHook = subdocument.querySelector('.containerCod')
+    const codigos = subdocument.querySelector('#cardsContainer')
 
     /** FUNCIONES INTERNAS DEL ELEMENTO**/
-   
+    BD_Codigos.forEach(obj => {
+        let componente = `
+            <div class="card dato-buscado">
+                <div class="additional">
+                    <div class="user-card">
+                        <div class="level center">
+                            Código
+                        </div>
+                        <div class="codi center">
+                            <i class="fa-solid fa-code"></i>
+                        </div>
+                        <div class="level center">
+                            ${obj.seccion}
+                        </div>
+                    </div>
+                    <div class="more-info">
+                        <h1>${obj.cod}</h1>
+                        <div class="coords">
+                            <span>Observación</span> <br><br>
+                            <p>${obj.observacion}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="general">
+                    <h1>${obj.cod}</h1>
+                    <div class="conti">
+                        <div class="servicios">
+                            <p><strong>Servicio</strong></p>
+                            <p>${obj.servicio}</p>
+                        </div>
+                        <div class="prestador">
+                            <p><strong>Prestador</strong></p>
+                            <p>${obj.prestador}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+        codigos.insertAdjacentHTML('beforeend', componente);
 
-    /** **/
+    })
+
+    document.querySelector('#inputSearch').addEventListener('keyup', (e) => {
+        buscarElemento(e.target.value);
+    });
 
     subdocument.querySelectorAll('.hook__hook--btn').forEach((button) => {
         button.addEventListener('click', () => {
             switch (button.name) {
                 case 'open':
-                    subdocument.classList.add('active')
-                    stylesParentElemInHook.classList.add('active')
-
-                    // document.querySelector('#searcherGuion').addEventListener('keyup', (e) => {
-                    //     buscarElemento(e.target.value)
-                    // })
-
-                    break
-                case 'close': {
-                    buscarElemento('')
-                    subdocument.classList.remove('active')
-                    stylesParentElemInHook.classList.remove('active')
-
+                    subdocument.classList.add('active');
+                    stylesParentElemInHook.classList.add('active');
                     break;
-                }
+                case 'close':
+                    buscarElemento('');
+                    subdocument.classList.remove('active');
+                    stylesParentElemInHook.classList.remove('active');
+                    break;
             }
-        })
-    })
+        });
+    });
     return subdocument
 }
---
+++ src/config/js/BD/BD_Codigos.js
@@ -0,0 +1,366 @@
+export const BD_Codigos = [
+  {
+    "seccion": "Viaticos",
+    "cod": "VIALC1",
+    "servicio": "Viaticos de alimentación cena",
+    "prestador": "7762-colmena seguros riesgos laborales",
+    "observacion": "Se liquida 1 cena+1almuerzo + taxis tarifas aeropuerto +1 taxi adicional. Asistencia a cita de calificación en junta nacional el dia 13 de enero de 2023 a las 8:30 amUsuario: Jhoan Sebastian Gomez, Fecha: 20/12/2022 5:13:18 p. m. se envia senda adm 590250 Coordinación de tiquetes + hospedaje habitación sencilla ingresando el 12/01/2023 y saliendo el 13/12/2022. Se liquida 1 cena + 1 almuerzo + taxis tarifas aeropuerto +1 taxi adicional. Asistencia a cita de calificación en junta nacional el dia 13 de enero de 2023 a las 8:30 am ... jhoan gomez"
+  },
+  {
+    "seccion": "Viaticos",
+    "cod": "VIALD1",
+    "servicio": "VIATICOS DE ALIMENTACION DESAYUNO",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Viaticos",
+    "cod": "VIAT01",
+    "servicio": "VIATICOS DE TRASPORTE",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Viaticos",
+    "cod": "VIAH02",
+    "servicio": "HOSPEDAJE HABITACION DOBLE",
+    "prestador": "5007-agencia de viajes y turismo aviatur",
+    "observacion": "se autoriza prorroga de hospedaje en habitación doble sin alimentación ingresando el dia 30/11/2023 y salida el dia 01/12/2023 en la ciudad de bogota , trabajadora se encuentra hospedada en el hotel macao desde el dia 26/11/2023 al 30/11/2023 solicitado en el senda anterior con el numero 638640. , se envia con fechas diferentes por traslapo en el senda no. 641390 .... jefferson vanegas"
+  },
+  {
+    "seccion": "Viaticos",
+    "cod": "S11302",
+    "servicio": "INTERNACION EN SERVICIO DE COMPLEJIDAD ALTA HABITACION BIPERSONAL",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Pric",
+    "cod": "931030",
+    "servicio": "INGRESO AL PRIC PROGRAMA REHABILITACION INTEGRAL COLMENA",
+    "prestador": "5007-agencia de viajes y turismo aviatur",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Pric",
+    "cod": "931000",
+    "servicio": "FISIOTERAPIA TERAPEUTICA INTEGRAL",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Pric",
+    "cod": "T10329",
+    "servicio": "ANALISIS PUESTO TRABAJO FUERA CIUDAD",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "C8902003",
+    "servicio": "CONSULTA ORTOPEDIA Y TRAUMATOLOGIA PRIMERA VEZ",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "940200",
+    "servicio": "ADMINISTRACION DE PRUEBA DE PERSONALIDAD CUALQUIER TIPO NCOC",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "C8903015",
+    "servicio": "CONSULTA MEDICINA DEL DOLOR CONTROL",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "C8902015",
+    "servicio": "CONSULTA MEDICINA DEL DOLOR PRIMERA VEZ",
+    "prestador": "6888-zerenia sas",
+    "observacion": "visto bueno del mgi del caso. Usuario: Jeisson Narvaez, Fecha: 28/11/2023 9:27:18 a. m. cita para el día 12/12/23 a las 09:20 con el dr. oscar fernando mejia villa la dirección av cra 19 # 106 - 30 barrio san patricio ips zerenia"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "C8903012",
+    "servicio": "CONSULTA PSIQUIATRIA CONTROL",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "C8902007",
+    "servicio": "CONSULTA DERMATOLOGIA PRIMERA VEZ",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "C8903007",
+    "servicio": "CONSULTA DERMATOLOGIA CONTROL",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "C8903011",
+    "servicio": "CONSULTA OFTALMOLOGIA CONTROL",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "C8902011",
+    "servicio": "CONSULTA OFTALMOLOGIA PRIMERA VEZ",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "89010503",
+    "servicio": "ENFERMERIA DOMICILIARIA 24 HORAS",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "D890105",
+    "servicio": "TURNO DE ENFERMERIA 24 HORAS ORDINARIAS",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "890106",
+    "servicio": "ATENCION VISITA DOMICILIARIA POR NUTRICION Y DIETETICA",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "D890116",
+    "servicio": "CURACION DOMICILIARIA MEDIANA SIN INSUMO",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "T111001",
+    "servicio": "ATENCION [VISITA] DOMICILIARIA POR FONOAUDILOGIA",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "T111105",
+    "servicio": "CONSULTA DE CONTROL O DE SEGUIMIENTO POR ESPECIALISTA EN ALERGOLOGÍA",
+    "prestador": "fundacion neumologica colombiana",
+    "observacion": "20/12/23 04:00"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "C8903012",
+    "servicio": "CONSULTA PSIQUIATRIA CONTROL",
+    "prestador": "6888-zerenia sas",
+    "observacion": "3104059392 // se comunica la señora estefani restrepo // clinica zerenia // admisiones // cu 5624749, solicita codigo de autorizacion de control de psiquiatria ordenada por el dr julian carvajal de zerenia el 27/07/2023, se verifica csao y se genera codigo de autorizacion y se informa. mateo ospina"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "C8902010",
+    "servicio": "CONSULTA NEUROCIRUGIA PRIMERA VEZ",
+    "prestador": "7135-medicadiz sas",
+    "observacion": "cita de control por neurocirugía consulta externa: se agenda cita con el doctor orlando lopez carvajar el día  23/10/2023 a las 13:00pm carrera 5 # 31   114 medicadiz"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "C8903010",
+    "servicio": "CONSULTA NEUROCIRUGIA CONTROL",
+    "prestador": "7184 - NEUROCIRUGIA ASOCIADA SAS",
+    "observacion": "/solicita codigo autorizacion  consulta control por neurtocirujia orden dr nestor cagoada  ips neurocirugia 5624698 codigo prestador  23 03 23...dadriana rodriguez"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "931111",
+    "servicio": "HIDROTERAPIA",
+    "prestador": "6530 - GRUPO CUIDAR S A S",
+    "observacion": "3115047547 // se comunica ximena lozada -- ips grupo cuidar sas -- auxiliar administrativa  solicitando autorizacion de  10 sesiones de hidro terapias     orden del dr   julio giraldo   valencia .fecha  11/07/2023 ips federico lleras acosta    --- inicia terapias el 29/07/2023 a las 09+00 am --- carrera 4 g  n 38a   01 barrio magisterio -al informar que debe de llevar la afiliada me indica que ellos se contactaran con la afiliada - jheison alvarez"
+  },
+  {
+    "seccion": "Consultas",
+    "cod": "C8903013",
+    "servicio": "CONSULTA UROLOGIA CONTROL",
+    "prestador": "8544-urologia funcional s.a.s",
+    "observacion": ""
+  },
+  {
+    "seccion": "Traslados",
+    "cod": "S31302",
+    "servicio": "AMBULANCIA TRASLADO TERRESTRE BASICO REDONDO BOGOTA",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Traslados",
+    "cod": "S33305",
+    "servicio": "AMBULANCIA TRASLADO TERRESTRE MEDICALIZADO SIMPLE OTROS MUNICIPIOS",
+    "prestador": "4918-ambulancias linea de fe s.a.s",
+    "observacion": "10:00am 14/06/2023. desde: clinica los rosales pereira. Hacia: clinica de occidente cali"
+  },
+  {
+    "seccion": "Traslados",
+    "cod": "TX100020",
+    "servicio": "TRASLADO TAXI URBANO BOGOTA HORA",
+    "prestador": "5610-compañia radio taxi unidos s,a,s,",
+    "observacion": "Traslado para el día 27/12/23 a las 08:00. desde: cra 7b # 134b  11 apto 807 condominio country reservado. Hasta: CALLE116 A #70 C 98 virtualfarma pontevedra"
+  },
+  {
+    "seccion": "Traslados",
+    "cod": "S32305",
+    "servicio": "TRASLADO SIMPLE",
+    "prestador": "5086-car asistencia sas",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Traslados",
+    "cod": "S32306",
+    "servicio": "TRASLADO REDONDO",
+    "prestador": "5086-car asistencia sas",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Traslados",
+    "cod": "TIQ001",
+    "servicio": "TIQUETE AEREO",
+    "prestador": "5008 - AGENCIA DE VIAJES Y TURISMO AVIATUR",
+    "observacion": "cita de calificación en junta nacional el dia 13 de enero de 2023 a las 8:30 am"
+  },
+  {
+    "seccion": "Traslados",
+    "cod": "TIQ001",
+    "servicio": "TIQUETE AEREO",
+    "prestador": "5007-agencia de viajes y turismo aviatur",
+    "observacion": "Usuario: Daniel Mauricio Reyes, Fecha: 14/11/2023 8:27:06 a. m. se envía senda adm 638186, se autoriza 4 tiquetes aereos desde valleduapar hacia bogota con retorno para el paciente y acompañante, saliendo el día 21/11/2023 a las 08:30am y retorno el 23/11/2023 10:00am + hospedaje doble en la ciudad de bogota ingresando el 21/11/2023 y saliendo el 23/11/2023, daniel reyes."
+  },
+  {
+    "seccion": "Reembolsos",
+    "cod": "RM001",
+    "servicio": "REEMBOLSO MEDICAMENTOS",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Reembolsos",
+    "cod": "RMO014",
+    "servicio": "REEMBOLSO DE TRASLADOS",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Reembolsos",
+    "cod": "RMO010",
+    "servicio": "REEMBOLSO INSUMOS",
+    "prestador": "Sin prestador",
+    "observacion": "Sin observacion"
+  },
+  {
+    "seccion": "Examenes",
+    "cod": "871121",
+    "servicio": "RADIOGRAFIA DE TORAX PA O AP Y LATERAL",
+    "prestador": "6887-instituto de diagnostico medico s.a.",
+    "observacion": "RADIOGRAFIA TORAX AP Y LATERAL fecha 21/11/2023 orden medica dr. Mauricio Duran - Usuario: Jeisson Narvaez, Fecha: 12/12/2023 8:33:18 a. m. se anula codigo ya que la paciente no puede asistir hasta no habersele realizado el examen broncoscopia Usuario: Jeisson Narvaez, Fecha: 7/12/2023 12:11:13 p. m. cita para el día 15/12/23 a las 09:30 en la dirección calle 76 # 15 - 55 ips idime clinica nueva el lago. Jeisson Narvaez"
+  },
+  {
+    "seccion": "Examenes",
+    "cod": "374701",
+    "servicio": "BRONCOSCOPIA FIBROOPTICA CON CEPILLADO BRONQUIAL",
+    "prestador": "6487-fundacion neumologica colombiana",
+    "observacion": "BRONCOSCOPIA BAJO ANESTESIA GENERAL   FIBROBRONCOSCOPIA CON LAVADO fecha 21/11/2023 orden medica dr. Mauricio Duran"
+  },
+  {
+    "seccion": "Examenes",
+    "cod": "901227",
+    "servicio": "MYCOBACTERIUM TUBERCULOSIS CULTIVO",
+    "prestador": "6487-fundacion neumologica colombiana",
+    "observacion": "ESTUDIOS DE MICROBIOLOGIA BRONCOSCOPIA BAJO ANESTESIA GENERAL Y LAVADO BRONQUIOLOALVEOLAR fecha 21/11/2023 orden medica dr. Mauricio Duran <br><br> - coloración de GRAM [homologada bajo código 901104   coloración de gram y lectura para cualquier muestra] <br><br> - cultivo para germenes comunes [homologada bajo código T112184   cultivo de germenes comunes y a/b (mic)]<br><br> - coloracion de acido alcohol resistente BK [homologada bajo código T10972   coloracion acido alcohol resistente [zielh nielsen] enmoco o linfa] <br><br>- cultivo para mycobacterias [homologada bajo código 901227   mycobacterium tuberculosis cultivo]<br><br> - coloracion de hongos KOH [homologada bajo código T11559   cultivo de hongos (incluye koh)]<br><br> - cultivo para hongos [homologada bajo código T112464   hongos: microscopico y cultivo]<br><br> - PCR para detección de M.Tuberculosis [homologada bajo código T111789   gen rpob (mycobacterium tuberculosis con deteccion de resistencia a la rifampicina por pcr] <br><br>- citología [homologada bajo código T112133   citologia por aspiracion con coloracion especial]<br><br> - coloración especial zn / PAS / GROCOTT [homologada bajo código T112152   coloracion especial cant 3]"
+  },
+  {
+    "seccion": "Examenes",
+    "cod": "893805",
+    "servicio": "ESPIROMETRIA O CURVA DE FLUJO VOLUMEN PRE Y POST BRONCODILATADORES",
+    "prestador": "fundacion neumologica colombiana",
+    "observacion": "12/12/2023  14:30 realización del examen"
+  },
+  {
+    "seccion": "Examenes",
+    "cod": "893819",
+    "servicio": "PRUEBA DE EJERCICIO CARDIO PULMONAR",
+    "prestador": "fundacion neumologica colombiana",
+    "observacion": "pruebas feno para los días 02   03   06   07"
+  },
+  {
+    "seccion": "Oxigenoterapia",
+    "cod": "T113164",
+    "servicio": "SERVICIO OXIGENO PORTATIL PERMANENTE",
+    "prestador": "7164-messer colombia s a",
+    "observacion": "SERVICIO OXIGENO PORTATIL PERMANENTE MES JUNIO /// MARIANA PADILLA"
+  },
+  {
+    "seccion": "Oxigenoterapia",
+    "cod": "893801",
+    "servicio": "CONSUMO DE OXIGENO Y PRODUCCION DE CO2 EN REPOSO",
+    "prestador": "4894 - AIR LIQUIDE  COLOMBIA SAS",
+    "observacion": "alquiler de concentracion de oxigeno domiciliario del mes de noviembre"
+  },
+  {
+    "seccion": "Oxigenoterapia",
+    "cod": "MI023569",
+    "servicio": "ALQUILER CONCENTRADOR O BALA OXIGENO MES",
+    "prestador": "7164-messer colombia s a",
+    "observacion": "Cordial saludo, Solicito de su colaboración con la generación de los codigos para el proveedor messer 050132056 RAMIREZ CRUZ, ELIANA CC 65775344 06/06/2022 IBAGUE COX   GOX   PORTGOX | CARRO   REGULADOR SERVICIO DE OXIGENOTERAPIA EN CONCENTRADOR 140.000 050132056 RAMIREZ CRUZ, ELIANA CC 65775344 06/06/2022 IBAGUE COX   GOX   PORTGOX | CARRO   REGULADOR SERVICIO OXIGENO PORTATIL PERMANENTE 66.000"
+  },
+  {
+    "seccion": "Oxigenoterapia",
+    "cod": "MD009379",
+    "servicio": "CONCENTRADOR OXIGENO MEDICINAL GASEOSO GASEOSO OXIGENOS_COLOMBIA",
+    "prestador": "7164-messer colombia s a",
+    "observacion": "Cordial saludo, Solicito de su colaboración con la generación de los codigos para el proveedor messer 050132056 RAMIREZ CRUZ, ELIANA CC 65775344 06/06/2022 IBAGUE COX   GOX   PORTGOX | CARRO   REGULADOR SERVICIO DE OXIGENOTERAPIA EN CONCENTRADOR 140.000 050132056 RAMIREZ CRUZ, ELIANA CC 65775344 06/06/2022 IBAGUE COX   GOX   PORTGOX | CARRO   REGULADOR SERVICIO OXIGENO PORTATIL PERMANENTE 66.000"
+  },
+  {
+    "seccion": "Juntas",
+    "cod": "T11007",
+    "servicio": "VALORACION DICTAMEN EQUIPO INTERDISCIPLINARIO",
+    "prestador": "5623531-rico becerra carlos hernando",
+    "observacion": "\"Ppc. Valoraci&#243;n enviada por Nydia Janeth Sepulveda   Soprote AT: 2744254\""
+  },
+  {
+    "seccion": "Juntas",
+    "cod": "T10322",
+    "servicio": "JUNTAS INTERDISCIPLINARIAS DE SALUD MENTAL",
+    "prestador": "7807-mutalis sas",
+    "observacion": "Usuario: Jeisson Narvaez, Fecha: 1/11/2023 2:52:37 p. m.Cita coordinada 1era prueba el día 28/11/23 a la 13:00 hasta las 16:00. Entrevista: para el día 29/11/23 a las 08:00 hasta 10:00. 2da prueba para el día 29/11/23 a las 10:00 hasta las 12:00. Recomendación especial de la junta, mgi del caso debe de enviarles un resumen del caso de la trabajadora y que requiere de ellos, debido a que la trabajadora ha tenido muchas reprogramaciones de esta cita."
+  },
+  {
+    "seccion": "Juntas",
+    "cod": "T11001",
+    "servicio": "CALIFICACION DE ORIGEN",
+    "prestador": "5622056-junta reg calificacion de invali nte santande",
+    "observacion": "Buen día: Por favor me generan un código de autorización para envío de caso a la junta. ATEL             2744254.              CATALINO RODRIGUEZ AGUILAR. Motivo Envío ORIGEN. Junta                NORTE SANTANDER."
+  },
+  {
+    "seccion": "Estancias",
+    "cod": "S1130101",
+    "servicio": "INTERNACION EN SERVICIO DE COMPLEJIDAD ALTA HABITACION UNIPERSONAL",
+    "prestador": "6620-hospital infantil san jose",
+    "observacion": "3143596866   3137394765// se comunica la sra lizeth aneira//hospital infantil universitario de san jose solicitando: dx: r521 dolor cronico   y450 efectos adversos de opeaseos   z723 problemas relaciones por la falta de ejercicio. internacion unipersonal alta complejidad desde el dia 26/08/2023 hasta el dia de hoy 01/09/2023. se genera codigo por los dias 29/30/31 de agosto 2023..."
+  },
+  {
+    "seccion": "Estancias",
+    "cod": "S1130401",
+    "servicio": "INTERNACION EN SERVICIO DE COMPLEJIDAD ALTA HABITACION DE CUATRO CAMAS",
+    "prestador": "5320-clinica de occidente s a",
+    "observacion": "6026083200se comunica carolina rios / clinica del occidente requiere autorizar prorroga de los dias 25, 26 y 27 de junio para habitacioncuatro camas complejidad alta motivo: remision ( arl en busqueda de remision al trabajador )-fredy forero-"
+  }
+]
