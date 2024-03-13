5e7dbce23d655e5536a94f166bf7e702165565da
AvendanoisPepe <jsavendano62@misena.edu.co>
2024-03-13 15:36:19 -0500

    version 1.0.1
    actualizacion de enlace

--- src/config/js/index.js
+++ src/config/js/index.js
@@ -1,172 +1,243 @@
 /** ESTILOS */
-import '@javascript/librerias/fontAwesome/css/fontawesome.css'
-import '@javascript/librerias/fontAwesome/css/brands.css'
-import '@javascript/librerias/fontAwesome/css/solid.css'
-import '@javascript/librerias/fontAwesome/css/regular.css'
-import 'animate.css';
-import '@styles/style.scss'
-import '@styles/_navbar.scss'
-import readXlsxFile from 'read-excel-file'
-// 
-import Quill from 'quill';
-import '@javascript/librerias/notas/quill.js'
-import '@styles/notas/core.css'
-import '@styles/notas/snow.css'
-// 
+import "@javascript/librerias/fontAwesome/css/fontawesome.css";
+import "@javascript/librerias/fontAwesome/css/brands.css";
+import "@javascript/librerias/fontAwesome/css/solid.css";
+import "@javascript/librerias/fontAwesome/css/regular.css";
+import "animate.css";
+import "@styles/style.scss";
+import "@styles/_navbar.scss";
+import readXlsxFile from "read-excel-file";
+//
+import Quill from "quill";
+import "@javascript/librerias/notas/quill.js";
+import "@styles/notas/core.css";
+import "@styles/notas/snow.css";
+//
 /** ESTANDARS */
-import {router} from '@router/index.routes.js' //*
-import {navListPop} from '@javascript/funcionales/alertas.js'
+import { router } from "@router/index.routes.js"; //*
+import { navListPop } from "@javascript/funcionales/alertas.js";
+import Swal from "sweetalert2";
 
-router(window.location.hash) //*
-window.addEventListener('hashchange', ()=> {
-    router(window.location.hash)
-})
+router(window.location.hash); //*
+window.addEventListener("hashchange", () => {
+  router(window.location.hash);
+});
 
 // export let iDDB = new Localbase('db_telefonica')
 
 //** LEER ARCHIVOS EXCEL */
 export const leerExcel = async (archivoExcel) => {
-    const content = await readXlsxFile(archivoExcel.files[0])
+  const content = await readXlsxFile(archivoExcel.files[0]);
 
-    const [headerRow, ...dataRows] = content;
-    const columnTitles = headerRow.map((title) => title.trim());
+  const [headerRow, ...dataRows] = content;
+  const columnTitles = headerRow.map((title) => title.trim());
 
-    const arrObjetos = dataRows.map((row) => {
-        const objeto = {};
-        row.forEach((value, index) => {
-          const title = columnTitles[index];
-          objeto[title] = value;
-        });
-        return objeto;
-      });
-}
+  const arrObjetos = dataRows.map((row) => {
+    const objeto = {};
+    row.forEach((value, index) => {
+      const title = columnTitles[index];
+      objeto[title] = value;
+    });
+    return objeto;
+  });
+};
 
 // video para base de datos local
-    // https://www.youtube.com/watch?v=KJnupY2HPCg&t=670s&ab_channel=MakeAppswithDanny
+// https://www.youtube.com/watch?v=KJnupY2HPCg&t=670s&ab_channel=MakeAppswithDanny
 // Libreria de base de datos local
-    // https://github.com/dannyconnell/localbase
+// https://github.com/dannyconnell/localbase
 
 //** END LEER ARCHIVOS EXCEL */
 
 /** NAVBAR */
 
 const navList = [
-    {
-        SEGMENTO: "MEDICALIZACION",
-        SUBLIST: ["Guiones", "Normativa"]
-    },
-    {
-        SEGMENTO: "OPCIONES",
-        SUBLIST: ["Códigos", "Glosario"]
-    },
-    {
-        SEGMENTO: "PREVENCION",
-        SUBLIST: ["Guiones", "Normativa"]
-    },
-    {
-        SEGMENTO: "SEGUROSVIDA",
-        SUBLIST: ["Guiones"]
-    }
-]
-
-const navItems = document.querySelectorAll('.route')
-const secondMenu = document.querySelector('#secondMenu')
-
-navItems.forEach((linkItem) => {
-
-    linkItem.addEventListener("click", () => {
-
-        const liItem = linkItem.parentNode
-        secondMenu.innerHTML = ''
-
-        const data = navList.filter(el=> el.SEGMENTO == liItem.getAttribute('name'))
-
-            if (data.length) {
-                data[0].SUBLIST.forEach(el=>{
-                    secondMenu.insertAdjacentHTML('afterbegin', `<li><a href="#/${data[0].SEGMENTO+el}" class="route">${el}</a></li>`)
-                })
-            }
-
-        // if (linkItem.name.includes('pop')) {
-        //     const lista = linkItem.name.split('_')[1]
-        //     navListPop(lista)
-        // }
-
-
-        navItems.forEach((ancor) => {
-            ancor.parentNode.classList.remove('active')
-            secondMenu.addEventListener("click", () => { 
-                secondMenu.innerHTML = ''
-                // linkItem.parentNode.classList.remove('active')
-            })
-        })
-        linkItem.parentNode.classList.add('active')
-    })
-})
-/** END NAVBAR */
-
-
-const btnMisNotas = document.getElementById('btnMisNotas')
-const editor = document.getElementById('editor')
-const fullEditor = document.getElementById('fullEditor')
-const copiMisNotas = document.getElementById('copiMisNotas')
-
-let toolbarOptions = [
-    [{ 'size': ['small', false, 'large', 'huge'] }],
-    ['bold', 'italic', 'underline'],
-    ['link'],
-    [{ 'header': 1 }],
-    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-    [{ 'align': [] }],
-    [{ 'indent': '-1' }, { 'indent': '+1' }],
-    [{ 'color': [] }, { 'background': [] }],
+  {
+    SEGMENTO: "MEDICALIZACION",
+    SUBLIST: ["Guiones", "Normativa"],
+  },
+  {
+    SEGMENTO: "OPCIONES",
+    SUBLIST: ["Códigos", "Glosario"],
+  },
+  {
+    SEGMENTO: "PREVENCION",
+    SUBLIST: ["Guiones", "Normativa"],
+  },
+  {
+    SEGMENTO: "SEGUROSVIDA",
+    SUBLIST: ["Guiones"],
+  },
 ];
 
+const navItems = document.querySelectorAll(".route");
+const secondMenu = document.querySelector("#secondMenu");
+
+navItems.forEach((linkItem) => {
+  linkItem.addEventListener("click", () => {
+    const liItem = linkItem.parentNode;
+    secondMenu.innerHTML = "";
+
+    const data = navList.filter(
+      (el) => el.SEGMENTO == liItem.getAttribute("name")
+    );
+
+    if (data.length) {
+      data[0].SUBLIST.forEach((el) => {
+        secondMenu.insertAdjacentHTML(
+          "afterbegin",
+          `<li><a href="#/${
+            data[0].SEGMENTO + el
+          }" class="route">${el}</a></li>`
+        );
+      });
+    }
+
+    // if (linkItem.name.includes('pop')) {
+    //     const lista = linkItem.name.split('_')[1]
+    //     navListPop(lista)
+    // }
+
+    navItems.forEach((ancor) => {
+      ancor.parentNode.classList.remove("active");
+      secondMenu.addEventListener("click", () => {
+        secondMenu.innerHTML = "";
+        // linkItem.parentNode.classList.remove('active')
+      });
+    });
+    linkItem.parentNode.classList.add("active");
+  });
+});
+/** END NAVBAR */
+
+const btnMisNotas = document.getElementById("btnMisNotas");
+const editor = document.getElementById("editor");
+const fullEditor = document.getElementById("fullEditor");
+const copiMisNotas = document.getElementById("copiMisNotas");
+
+let toolbarOptions = [
+  [{ size: ["small", false, "large", "huge"] }],
+  ["bold", "italic", "underline"],
+  ["link"],
+  [{ header: 1 }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ align: [] }],
+  [{ indent: "-1" }, { indent: "+1" }],
+  [{ color: [] }, { background: [] }],
+];
 
 // Cuadro de texto enriquecido 'Mis notas'
-let quill = new Quill('#editor', {
-    modules: {
-        toolbar: toolbarOptions,
-    },
-    theme: 'snow'
+let quill = new Quill("#editor", {
+  modules: {
+    toolbar: toolbarOptions,
+  },
+  theme: "snow",
 });
 
-
 // Mis notas se ejecuta visible entonces lo ocutalmos
-fullEditor.style.display = 'none'
+fullEditor.style.display = "none";
 // al cargar la pagina lee el portapapeles y traemos lo que este en el
-let dataRecuperado = sessionStorage.getItem("misNotas")
-let newContents = JSON.parse(dataRecuperado)
+let dataRecuperado = sessionStorage.getItem("misNotas");
+let newContents = JSON.parse(dataRecuperado);
 // se le asigna el valor del portapaples en el texto enriquecido
-quill.setContents(newContents)
+quill.setContents(newContents);
 
 const saveSession = () => {
-    let contenido = quill.getContents()
+  let contenido = quill.getContents();
 
-    // convierto el objeto en string para luego almacenarlo en el portapapeles
-    let objString = JSON.stringify(contenido)
-    sessionStorage.setItem('misNotas', objString)
+  // convierto el objeto en string para luego almacenarlo en el portapapeles
+  let objString = JSON.stringify(contenido);
+  sessionStorage.setItem("misNotas", objString);
 
-    quill.deleteText(0, 10000)
-    quill.setContents(contenido)
-}
+  quill.deleteText(0, 10000);
+  quill.setContents(contenido);
+};
 
 const toggleNotes = (e) => {
-    if (e.target.className.includes('active')) {
-        e.target.classList.remove('active')
-        fullEditor.style.display = 'none'
-        saveSession()
-    } else {
-        e.target.classList.add('active')
-        fullEditor.removeAttribute('style')
-    }
-}
+  if (e.target.className.includes("active")) {
+    e.target.classList.remove("active");
+    fullEditor.style.display = "none";
+    saveSession();
+  } else {
+    e.target.classList.add("active");
+    fullEditor.removeAttribute("style");
+  }
+};
 
 const copiarMisNotas = () => {
-    quill.setSelection(0, 10000)
-    document.execCommand('copy')
-}
+  quill.setSelection(0, 10000);
+  document.execCommand("copy");
+};
 
-btnMisNotas.addEventListener('click', toggleNotes)
-copiMisNotas.addEventListener('click', copiarMisNotas)
-window.addEventListener('beforeunload', saveSession);
+btnMisNotas.addEventListener("click", toggleNotes);
+copiMisNotas.addEventListener("click", copiarMisNotas);
+window.addEventListener("beforeunload", saveSession);
+
+// END CONSUMO API
+const setConfigIndex = (props) => {
+  const { activeSQL_API } = props;
+
+  if (activeSQL_API) {
+    const sendForm = document.getElementById("sendForm");
+
+    if (!sessionStorage.getItem("session")) {
+      sendForm.parentNode.classList.remove("hide");
+    }
+
+    sendForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const data = {
+        usuario: e.target.elements[0].value,
+        campana: e.target.elements[1].value,
+        modulo: e.target.elements[2].value,
+        observaciones: e.target.elements[3].value,
+      };
+
+      e.target.parentNode.remove();
+      Swal.fire({
+        icon: "info",
+        title: "Enviando",
+        heightAuto: false,
+        didOpen: () => {
+          Swal.showLoading();
+        },
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+      });
+      fetch(
+        "http://colbogweb20:8081/Webservices_Simulador/api/main/insUpdTransaccion",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(data),
+        }
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          sessionStorage.setItem("session", true);
+          sendForm.parentNode.classList.add("hide");
+          Swal.fire({
+            heightAuto: false,
+            icon: "success",
+            title: "Datos Enviados correctamente.",
+            allowOutsideClick: true,
+          });
+        })
+        .catch((error) => {
+          sessionStorage.setItem("session", false);
+          Swal.fire({
+            icon: "error",
+            title: "Ocurrió un error durante el consumo del API",
+            heightAuto: false,
+            text: error,
+            allowOutsideClick: true,
+          });
+        });
+    });
+  }
+};
+
+setConfigIndex({
+  activeSQL_API: true,
+});
